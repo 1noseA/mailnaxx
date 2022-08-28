31f0b249abeeacbb8a45a61b5c0880ca60c9c1ac
1noseA <1noseA2L@gmail.com>
2022-08-28 23:41:10 +0900

    first commit

---
+++ src/main/java/com/mailnaxx/MailnaxxApplication.java
@@ -0,0 +1,13 @@
+package com.mailnaxx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailnaxxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailnaxxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mailnaxx/MailnaxxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mailnaxx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailnaxxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
