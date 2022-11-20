39afbba3f2fa6b4488c12235315154e4a847a32d
1noseA <1noseA2L@gmail.com>
2022-11-20 19:34:13 +0900

    他の項目にも反映するも勝手にスクロールされてしまう

--- src/main/java/com/mailnaxx/form/UsersForm.java
+++ src/main/java/com/mailnaxx/form/UsersForm.java
@@ -13,97 +13,99 @@ import lombok.Data;
 public class UsersForm {
 
     // 氏名(漢字)_姓
-    @NotBlank(groups = ValidGroup1.class, message = "氏名(漢字)_姓を入力してください")
+    @NotBlank(groups = ValidGroup1.class, message = "入力してください")
     @Size(max=10, groups = ValidGroup2.class, message = "全角10文字以内で入力してください")
     @Pattern(regexp="^[^ -~｡-ﾟ]+$", groups = ValidGroup3.class, message = "全角文字で入力してください")
     private String userLastName;
 
     // 氏名(漢字)_名
-    @NotBlank(groups = ValidGroup1.class, message = "氏名(漢字)_名を入力してください")
+    @NotBlank(groups = ValidGroup1.class, message = "入力してください")
     @Size(max=10, groups = ValidGroup2.class, message = "全角10文字以内で入力してください")
     @Pattern(regexp="^[^ -~｡-ﾟ]+$", groups = ValidGroup3.class, message = "全角文字で入力してください")
     private String userFirstName;
 
     // 氏名(カタカナ)_姓
-    @NotBlank(groups = ValidGroup1.class, message = "氏名(カタカナ)_姓を入力してください")
+    @NotBlank(groups = ValidGroup1.class, message = "入力してください")
     @Size(max=10, groups = ValidGroup2.class, message = "全角10文字以内で入力してください")
     @Pattern(regexp="^[ァ-ー]+$", groups = ValidGroup3.class, message = "全角カナで入力してください")
     private String userLastKana;
 
     // 氏名(カタカナ)_名
-    @NotBlank(groups = ValidGroup1.class, message = "氏名(カタカナ)_名を入力してください")
+    @NotBlank(groups = ValidGroup1.class, message = "入力してください")
     @Size(max=10, groups = ValidGroup2.class, message = "全角10文字以内で入力してください")
     @Pattern(regexp="^[ァ-ー]+$", groups = ValidGroup3.class, message = "全角カナで入力してください")
     private String userFirstKana;
 
     // 入社年月_年
-    @Positive(message = "入社年月_年を入力してください")
+    // @Positive(message = "入社年月_年を選択してください")
     private int hireYear;
 
     // 入社年月_月
-    @Positive(message = "入社年月_年を入力してください")
+    // @Positive(message = "入社年月_年を選択してください")
     private int hireMonth;
 
     // 所属
-    @PositiveOrZero(message = "所属を入力してください")
+    @PositiveOrZero(groups = ValidGroup1.class, message = "選択してください")
     private int affiliationId;
 
     // 権限区分
-    @NotBlank(message = "権限区分を入力してください")
+    // @NotBlank(message = "権限区分を選択してください")
     private String roleClass;
 
     // 営業担当
     private String salesFlg;
 
     // 生年月日_年
-    @Positive(message = "生年月日_年を入力してください")
+    @Positive(groups = ValidGroup1.class, message = "選択してください")
     private int birthYear;
 
     // 生年月日_月
-    @Positive(message = "生年月日_月を入力してください")
+    @Positive(groups = ValidGroup1.class, message = "選択してください")
     private int birthMonth;
 
     // 生年月日_日
-    @Positive(message = "生年月日_日を入力してください")
+    @Positive(groups = ValidGroup1.class, message = "選択してください")
     private int birthDay;
 
     // 郵便番号1
-    @NotBlank(message = "郵便番号1を入力してください")
+    @NotBlank(groups = ValidGroup1.class, message = "入力してください")
+    @Pattern(regexp="^[0-9]+$", groups = ValidGroup2.class, message = "数字で入力してください")
     private String postCode1;
 
     // 郵便番号2
-    @NotBlank(message = "郵便番号2を入力してください")
+    @NotBlank(groups = ValidGroup1.class, message = "入力してください")
+    @Pattern(regexp="^[0-9]+$", groups = ValidGroup2.class, message = "数字で入力してください")
     private String postCode2;
 
     // 住所
-    @NotBlank(groups = ValidGroup1.class, message = "住所を入力してください")
+    @NotBlank(groups = ValidGroup1.class, message = "入力してください")
     @Size(max=256, groups = ValidGroup2.class, message = "全角256文字以内で入力してください")
     @Pattern(regexp="^[^ -~｡-ﾟ]+$", groups = ValidGroup3.class, message = "全角文字で入力してください")
     private String address;
 
     // 電話番号1
-    @NotBlank(groups = ValidGroup1.class, message = "電話番号1を入力してください")
-    @Pattern(regexp="^[0-9]+$", groups = ValidGroup2.class, message = "電話番号1は数字5桁以内で入力してください")
+    @NotBlank(groups = ValidGroup1.class, message = "入力してください")
+    @Pattern(regexp="^[0-9]+$", groups = ValidGroup2.class, message = "数字で入力してください")
     private String phoneNumber1;
 
     // 電話番号2
-    @NotBlank(groups = ValidGroup1.class, message = "電話番号2を入力してください")
-    @Pattern(regexp="^[0-9]+$", groups = ValidGroup2.class, message = "電話番号2は数字4桁以内で入力してください")
+    @NotBlank(groups = ValidGroup1.class, message = "入力してください")
+    @Pattern(regexp="^[0-9]+$", groups = ValidGroup2.class, message = "数字で入力してください")
     private String phoneNumber2;
 
     // 電話番号3
-    @NotBlank(groups = ValidGroup1.class, message = "電話番号3を入力してください")
-    @Pattern(regexp="^[0-9]+$", groups = ValidGroup2.class, message = "電話番号3は数字4桁以内で入力してください")
+    @NotBlank(groups = ValidGroup1.class, message = "入力してください")
+    @Pattern(regexp="^[0-9]+$", groups = ValidGroup2.class, message = "数字で入力してください")
     private String phoneNumber3;
 
     // メールアドレス
-    @NotBlank(groups = ValidGroup1.class, message = "メールアドレスを入力してください")
+    @NotBlank(groups = ValidGroup1.class, message = "入力してください")
     @Size(max=128, groups = ValidGroup2.class, message = "半角128文字以内で入力してください")
     @Email(groups = ValidGroup3.class, message = "メールアドレスの形式が間違っています")
     private String emailAddress;
 
     // パスワード
-    @NotBlank(groups = ValidGroup1.class, message = "パスワードを入力してください")
+    @NotBlank(groups = ValidGroup1.class, message = "入力してください")
     @Size(min=8, max=10, groups = ValidGroup2.class, message = "半角英数字8文字以上10文字以内で入力してください")
     @Pattern(regexp="^[A-Za-z0-9]+$", groups = ValidGroup3.class, message = "半角英数字で入力してください")
     private String password;
