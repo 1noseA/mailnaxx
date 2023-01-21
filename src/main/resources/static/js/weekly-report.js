83885072ec3306782023f1950cf66e5b3a803568
1noseA <1noseA2L@gmail.com>
2023-01-21 23:36:53 +0900

    スクロール無効化完了

--- src/main/resources/static/js/weekly-report.js
+++ src/main/resources/static/js/weekly-report.js
@@ -1,15 +1,6 @@
 $(function () {
-    // 入力欄押下時にスクロール（ページトップ遷移）を無効化(focusだとダメだった)
-    /*$('input, select, textarea').on('click', function() {
-        event.preventDefault();
-    });*/
-    // というかどこ押してもページトップに行ってしまうので無効化
+    // クリック時に移動してしまうのを無効化
     $('div').on('click', function() {
         event.preventDefault();
     });
-    // ボタン押下時にスクロールを無効化
-    $('button').on('click', function() {
-        event.preventDefault();
-    });
 });
-
