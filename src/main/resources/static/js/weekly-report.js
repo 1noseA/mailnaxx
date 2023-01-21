a119ae27e7b19332668db808961f15835a7ace6b
1noseA <1noseA2L@gmail.com>
2023-01-21 23:32:27 +0900

    入力欄押下時ページトップに移動する・モーダル閉じる時移動する不具合解消

--- src/main/resources/static/js/weekly-report.js
+++ src/main/resources/static/js/weekly-report.js
@@ -1,5 +1,15 @@
-/*$(function () {
-    $('.btnSub').on('click', () => {
-        $('#showColleaguesForm').toggle();
+$(function () {
+    // 入力欄押下時にスクロール（ページトップ遷移）を無効化(focusだとダメだった)
+    /*$('input, select, textarea').on('click', function() {
+        event.preventDefault();
+    });*/
+    // というかどこ押してもページトップに行ってしまうので無効化
+    $('div').on('click', function() {
+        event.preventDefault();
     });
-});*/
+    // ボタン押下時にスクロールを無効化
+    $('button').on('click', function() {
+        event.preventDefault();
+    });
+});
+
