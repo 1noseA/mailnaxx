3d391bf09c30fd8c955fee9ed019637362ad7ac6
1noseA <1noseA2L@gmail.com>
2023-04-01 23:51:23 +0900

    検索ボタンが効かなくなりコメントアウト

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -11,9 +11,9 @@ function showDetail(id) {
     form.submit();
 }
 
-$(function () {
+/*$(function () {
     // クリック時に移動してしまうのを無効化
     $('div').on('click', function() {
         event.preventDefault();
     });
-});
+});*/
