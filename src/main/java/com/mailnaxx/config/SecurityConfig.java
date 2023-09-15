4990c38570f4663fe0f959e349fbbe69699f76ea
1noseA <1noseA2L@gmail.com>
2023-09-15 16:30:30 +0900

    ヘッダーのログイン情報取得を反映（共通化したかったが不明）

--- src/main/java/com/mailnaxx/config/SecurityConfig.java
+++ src/main/java/com/mailnaxx/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.mailnaxx;
+package com.mailnaxx.config;
 
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
